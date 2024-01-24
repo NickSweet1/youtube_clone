3eed5c2178fc14bac015cf8b4005cf9750ef05ac
Nick <nicksweetj@gmail.com>
2024-01-24 15:03:45 -0500

    search function is working

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { Videos } from "./";
+import { useParams } from 'react-router-dom';
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const SearchFeed = () => {
-  return (
-    <div>SearchFeed</div>
-  )
-}
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
 
-export default SearchFeed
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography cariant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search Results for: <span style={{ color: "#FC1503" }}>{searchTerm}</span>
+      </Typography>
+
+      <Videos videos={videos} />
+    </Box>
+  );
+};
+
+export default SearchFeed;
